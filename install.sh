54a55740cd4876a09a64f83052b6f6e62b047de9
dzlzh <dzlzh@null.net>
2019-08-04 17:56:22 +0800

    :wrench: install.sh

--- install.sh
+++ install.sh
@@ -6,3 +6,16 @@ ln -s `pwd`/tmux/.tmux.conf ~/.tmux.conf
 
 # rime
 ln -s `pwd`/rime ~/.config/fcitx/rime
+
+# i3wm
+ln -s `pwd`/i3wm/i3 ~/.i3
+
+# URxvt
+ln -s `pwd`/URxvt/.Xresources ~/.Xresources
+
+# .xprofile
+ln -s `pwd`/config/.xprofile ~/.xprofile
+
+# git
+ln -s `pwd`/git/.gitconfig ~/.gitconfig
+
