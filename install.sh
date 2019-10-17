72ee4b1da12db706fc3939bfd2a810bf43a634c3
dzlzh <dzlzh@null.net>
2019-10-17 18:44:10 +0800

    install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,29 @@
+# polybar
+ln -s `pwd`/polybar ~/.config/polybar
+
+# dunst
+ln -s `pwd`/dunst ~/.config/dunst
+
+# i3wm
+ln -s `pwd`/i3wm/i3 ~/.i3
+
+# rime
+ln -s `pwd`/rime ~/.config/fcitx/rime
+
+# .xprofile
+ln -s `pwd`/config/.xprofile ~/.xprofile
+
+# URxvt
+ln -s `pwd`/URxvt/.Xresources ~/.Xresources
+
+# zsh
+ln -s `pwd`/config/.zshrc ~/.zshrc
+
 # nvim
 ln -s `pwd`/nvim ~/.config/nvim
 
 # tmux
 ln -s `pwd`/tmux/.tmux.conf ~/.tmux.conf
 
-# rime
-ln -s `pwd`/rime ~/.config/fcitx/rime
-
-# i3wm
-ln -s `pwd`/i3wm/i3 ~/.i3
-
-# URxvt
-ln -s `pwd`/URxvt/.Xresources ~/.Xresources
-
-# .xprofile
-ln -s `pwd`/config/.xprofile ~/.xprofile
-
 # git
 ln -s `pwd`/git/.gitconfig ~/.gitconfig
-
-# zsh
-ln -s `pwd`/config/.zshrc ~/.zshrc
