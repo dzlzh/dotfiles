849dfdd3070b2d5a36e4903712058f55f87d85b0
dzlzh <dzlzh@null.net>
2019-11-18 23:30:34 +0800

    termite

--- install.sh
+++ install.sh
@@ -1,20 +1,5 @@
-# polybar
-ln -s `pwd`/polybar ~/.config/polybar
-
-# dunst
-ln -s `pwd`/dunst ~/.config/dunst
-
 # i3wm
 ln -s `pwd`/i3wm/i3 ~/.i3
-
-# rime
-ln -s `pwd`/rime ~/.config/fcitx/rime
-
-# .xprofile
-ln -s `pwd`/config/.xprofile ~/.xprofile
-
-# URxvt
-ln -s `pwd`/URxvt/.Xresources ~/.Xresources
 
 # zsh
 ln -s `pwd`/config/.zshrc ~/.zshrc
@@ -28,6 +13,24 @@ ln -s `pwd`/tmux/.tmux.conf ~/.tmux.conf
 # git
 ln -s `pwd`/git/.gitconfig ~/.gitconfig
 ln -s `pwd`/git/.gitmessage ~/.gitmessage
+
+# rime
+ln -s `pwd`/rime ~/.config/fcitx/rime
+
+# polybar
+ln -s `pwd`/polybar ~/.config/polybar
+
+# dunst
+ln -s `pwd`/dunst ~/.config/dunst
+
+# .xprofile
+ln -s `pwd`/config/.xprofile ~/.xprofile
+
+# termite
+ln -s `pwd`/termite ~/.config/termite
+
+# URxvt
+ln -s `pwd`/URxvt/.Xresources ~/.Xresources
 
 # mycli
 ln -s `pwd`/config/.mycli ~/.mycli
