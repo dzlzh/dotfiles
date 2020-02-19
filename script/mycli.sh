99437c578ed9efd3f3a9fb5ea8c55a3439a25c71
dzlzh <dzlzh@null.net>
2020-02-19 16:03:20 +0800

    script

--- script/mycli.sh
+++ script/mycli.sh
@@ -5,7 +5,7 @@ set username [lindex $argv 1]
 set port     [lindex $argv 2]
 set password [lindex $argv 3]
 
-spawn mycli -h $hostname -u $username -P $port -p
-expect "*password*"
+spawn mycli -h $hostname -u $username -P $port
+expect "*Password:*"
 send "$password\r"
 interact
