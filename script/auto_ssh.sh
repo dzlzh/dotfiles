1b7d0f8c1b62eb2c502172dfb5309e2b5add3e75
dzlzh <dzlzh@null.net>
2020-03-04 19:10:08 +0800

    script

--- script/auto_ssh.sh
+++ script/auto_ssh.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/expect
-set timeout 10  
-set username [lindex $argv 0]  
-set password [lindex $argv 1]  
-set hostname [lindex $argv 2]  
+set timeout 10
+set username [lindex $argv 0]
+set password [lindex $argv 1]
+set hostname [lindex $argv 2]
 
-spawn ssh $username@$hostname
+set port 22
+if {$argc > 3} {
+    set port [lindex $argv 3]
+}
+
+spawn ssh $username@$hostname -p $port
 expect "*password*"
 send "$password\r"
 interact
