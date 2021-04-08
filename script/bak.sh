93f355ee79e4716e2caa5abf58204bd394950b51
dzlzh <8456085+dzlzh@users.noreply.github.com>
2021-04-08 18:35:24 +0800

    script

---
+++ script/bak.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#获得当前的时间
+ctime=$(date  +%Y%m%d%H%M%S)
+
+#备份/var/log目录
+tar czf /backup/log_${ctime}.tar.gz /var/log
+
+#删除7天前的备份文件
+find /backup -mtime +7 -type f -name "*.tar.gz" -exec  rm  -rf  {} \;
