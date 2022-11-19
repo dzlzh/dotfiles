6e10516f6e46608bd21c3defa0016ea54358c940
dzlzh <8456085+dzlzh@users.noreply.github.com>
2022-11-19 13:53:21 +0800

    install

--- install.sh
+++ install.sh
@@ -1,47 +1,40 @@
-# i3wm
-ln -s `pwd`/i3wm/.i3 ~/.i3
-
 # zsh
-ln -s `pwd`/zsh/.zshrc ~/.zshrc
-ln -s `pwd`/zsh/.p10k.zsh ~/.p10k.zsh
-
-# nvim
-ln -s `pwd`/nvim ~/.config/nvim
-
-# tmux
-ln -s `pwd`/tmux/.tmux.conf ~/.tmux.conf
-
-# termite
-ln -s `pwd`/termite ~/.config/termite
-
-# tilda
-ln -s `pwd`/tilda ~/.config/tilda
-
-# git
-ln -s `pwd`/git/.gitconfig ~/.gitconfig
-ln -s `pwd`/git/.gitmessage ~/.gitmessage
-
-# rime
-ln -s `pwd`/rime ~/.config/fcitx/rime
-
-# polybar
-ln -s `pwd`/polybar ~/.config/polybar
-
-# dunst
-ln -s `pwd`/dunst ~/.config/dunst
+ln -s `pwd`/zsh/.zshrc $HOME/.zshrc
 
 # .xprofile .profile
-ln -s `pwd`/config/.profile ~/.profile
-ln -s `pwd`/config/.xprofile ~/.xprofile
+ln -s `pwd`/config/.profile $HOME/.profile
+ln -s `pwd`/config/.xprofile $HOME/.xprofile
 
-# URxvt
-ln -s `pwd`/URxvt/.Xresources ~/.Xresources
+# i3wm
+ln -s `pwd`/i3wm/.i3 $HOME/.i3
 
-# mycli
-# ln -s `pwd`/config/.myclirc ~/.myclirc
+# rofi
+ln -s `pwd`/rofi $HOME/.config/rofi
+
+# polybar
+ln -s `pwd`/polybar $HOME/.config/polybar
+
+# dunst
+ln -s `pwd`/dunst $HOME/.config/dunst
 
 # redshift
-ln -s `pwd`/redshift ~/.config/redshift
+ln -s `pwd`/redshift $HOME/.config/redshift
 
-# starship
-ln -s `pwd`/config/starship.toml ~/.config/starship.toml
+# alacritty
+ln -s `pwd`/alacritty $HOME/.config/alacritty
+
+# tilda
+ln -s `pwd`/tilda $HOME/.config/tilda
+
+# tmux
+ln -s `pwd`/tmux/.tmux.conf $HOME/.tmux.conf
+
+# git
+ln -s `pwd`/git/.gitconfig $HOME/.gitconfig
+ln -s `pwd`/git/.gitmessage $HOME/.gitmessage
+
+# nvim
+ln -s `pwd`/nvim $HOME/.config/nvim
+
+# rime
+ln -s `pwd`/rime $HOME/.local/share/fcitx5/rime
