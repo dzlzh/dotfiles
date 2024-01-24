ce38df4822034f823f0171b8b892c6c439a2dfd0
dzlzh <dzlzh@duck.com>
2024-01-24 17:01:00 +0800

    nvim

--- .config/nvim/lua/plugins/common/telescope.lua
+++ .config/nvim/lua/plugins/common/telescope.lua
@@ -5,6 +5,7 @@ return {
     keys = {
         { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
         { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Find Grep" },
+        { "<leader>fh", "<cmd>Telescope oldfiles<cr>", desc = "Find Open History" },
     },
     opts = {
         defaults = {
