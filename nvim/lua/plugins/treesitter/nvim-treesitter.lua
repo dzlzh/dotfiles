cd28d7fa0f49b7faad4697c4f22735a881994459
dzlzh <dzlzh@duck.com>
2024-02-02 14:39:45 +0800

    nvim

--- nvim/lua/plugins/treesitter/nvim-treesitter.lua
+++ nvim/lua/plugins/treesitter/nvim-treesitter.lua
@@ -1,6 +1,6 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    version = false, -- last release is way too old and doesn't work on Windows
+    version = false,
     build = ":TSUpdate",
     event = { "BufReadPost", "BufNewFile" },
     config = function()
