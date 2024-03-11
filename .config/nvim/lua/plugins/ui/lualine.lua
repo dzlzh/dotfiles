76d15cdc16c1bfb5a1075ba727d2975f6c96a9b4
0xE7A38A <8456085+dzlzh@users.noreply.github.com>
2024-03-11 11:39:31 +0800

    Update lualine.lua

--- .config/nvim/lua/plugins/ui/lualine.lua
+++ .config/nvim/lua/plugins/ui/lualine.lua
@@ -1,6 +1,6 @@
 return {
     "nvim-lualine/lualine.nvim",
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     event = "VeryLazy",
-    opts = { theme = "auto" },
+    opts = { options = { disabled_filetypes = {"xs"} } },
 }
