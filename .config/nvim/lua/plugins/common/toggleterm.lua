874a4383ac7a18354e1a07d634167ce75aa98aed
0xE7A38A <8456085+dzlzh@users.noreply.github.com>
2024-04-19 15:51:30 +0800

    Update toggleterm.lua

--- .config/nvim/lua/plugins/common/toggleterm.lua
+++ .config/nvim/lua/plugins/common/toggleterm.lua
@@ -3,7 +3,7 @@ return {
     version = "*",
     event = "VeryLazy",
     opts = {
-        open_mapping = [[<c-\>]],
+        open_mapping = [[<Leader>sh]],
         start_in_insert = true,
         direction = "float",
         on_open = function ()
