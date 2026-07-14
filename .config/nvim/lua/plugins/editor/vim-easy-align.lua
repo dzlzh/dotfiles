72c31a1f1cfc1f2a42861768802031d005c5cd1f
git <git@git-scm.com>
2026-07-14 14:50:09 +0800

    nvim

--- .config/nvim/lua/plugins/editor/vim-easy-align.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "junegunn/vim-easy-align",
-    event = "VeryLazy",
-    config = function()
-        vim.keymap.set("x", "ga", "<Plug>(EasyAlign)", { remap = true, desc = "Easy Align" })
-    end,
-}
